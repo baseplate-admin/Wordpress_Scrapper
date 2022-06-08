2a4a307637162ab2f0e6c336b97600c9961ca834
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2022-06-08 13:50:21 +0600

    bump

--- json_to_csv.py
+++ json_to_csv.py
@@ -7,13 +7,18 @@ JSON = json.load(open("./posts.json", "r+", encoding="utf-8"))
 data_dict = []
 
 for item in JSON:
+    content = []
+    content_soup = BeautifulSoup(item["content"]["rendered"], features="html.parser")
+    for i in content_soup.find_all("p", attrs={"class": "more"}):
+        i.decompose()
+
+    content.append(str(content_soup))
+
     item_dict = {}
     item_dict["title"] = BeautifulSoup(
         item["title"]["rendered"], features="html5lib"
     ).get_text()
-    item_dict["content"] = BeautifulSoup(
-        item["content"]["rendered"], features="html5lib"
-    ).get_text()
+    item_dict["content"] = "".join(content)
     data_dict.append(item_dict)
 
 csv_file = open("test.csv", "w+")
